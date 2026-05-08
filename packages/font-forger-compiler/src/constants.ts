5544df13d2e73f198f1c686dbe1c34d8f63e31d9
yu.pan <panyupy@vip.qq.com>
2026-05-08 16:01:48 +0800

    feat: 加载字体文件

--- packages/font-forger-compiler/src/constants.ts
+++ packages/font-forger-compiler/src/constants.ts
@@ -1,7 +1,11 @@
-import findCacheDirectory from 'find-cache-directory'
 import path from 'path'
+import findCacheDirectory from 'find-cache-directory'
 
+/**
+ * 缓存目录
+ */
 export const CACHE_DIR = findCacheDirectory({ name: '@font-forger/compiler' }) || path.join(__dirname, '../node_modules/.cache')
+
 /**
  * 原始字体资源路径
  */
@@ -12,4 +16,7 @@ export const SOURCE_FONT_ASSET_PATH = path.join(CACHE_DIR, 'source-font')
  */
 export const SPLIT_FONT_ASSET_PATH = path.join(CACHE_DIR, 'split-font')
 
+/**
+ * 默认剩余字符块大小
+ */
 export const DEFAULT_REMAINING_CHUNK_SIZE = 1000
--- packages/font-forger-compiler/src/load-font-asset.ts
+++ packages/font-forger-compiler/src/load-font-asset.ts
@@ -1,3 +1,60 @@
-export const loadFontAsset = (path: string) => {
-    return ''
+import got from 'got'
+import path from 'path'
+import crypto from 'crypto'
+import fs from 'fs'
+import { pipeline } from 'stream/promises'
+import { SOURCE_FONT_ASSET_PATH } from './constants'
+
+const loadRemoteFontAsset = async (assetPath: string) => {
+  const ext = path.extname(assetPath)
+  const pathHash = crypto.createHash('sha256').update(assetPath).digest('hex')
+  const tempFilePath = path.join(SOURCE_FONT_ASSET_PATH, `${pathHash}${ext}`)
+
+  // 下载字体资产
+  const stream = got.stream(assetPath)
+  await fs.promises.mkdir(SOURCE_FONT_ASSET_PATH, { recursive: true })
+  await pipeline(stream, fs.createWriteStream(tempFilePath))
+
+  // 重命名临时资产到目标路径
+  const buffer = fs.readFileSync(tempFilePath)
+  const fileHash = crypto.createHash('sha256').update(buffer).digest('hex')
+  const finalFilePath = path.join(SOURCE_FONT_ASSET_PATH, `${fileHash}${ext}`)
+  fs.renameSync(tempFilePath, finalFilePath)
+  return finalFilePath
+}
+
+const loadLocalFontAsset = (assetPath: string) => {
+  const ext = path.extname(assetPath)
+  const filePath = path.isAbsolute(assetPath) ? assetPath : path.resolve(process.cwd(), assetPath)
+  const isFilePathAccessible = fs.existsSync(filePath)
+  if (!isFilePathAccessible) throw new Error(`splitFontFile: local source does not exist: ${filePath}`)
+
+  // 拷贝文件
+  const buffer = fs.readFileSync(filePath)
+  const fileHash = crypto.createHash('sha256').update(buffer).digest('hex')
+  const finalFilePath = path.join(SOURCE_FONT_ASSET_PATH, `${fileHash}${ext}`)
+  fs.copyFileSync(filePath, finalFilePath)
+  return finalFilePath
+}
+
+const extMap:Record<string, string> = {
+  'font/woff2': 'woff2',
+  'font/woff': 'woff',
+  'font/ttf': 'ttf'
+}
+
+const loadBase64Asset = (assetPath: string) => {
+  const [_, mime, base64] = assetPath.match(/^data:([^;]+);base64,(.+)$/) || []
+  if (!mime || !base64) throw new Error(`splitFontFile: base64 asset path is invalid: ${assetPath}`)
+  const buffer = Buffer.from(base64, 'base64')
+  const fileHash = crypto.createHash('sha256').update(buffer).digest('hex')
+  const finalFilePath = path.join(SOURCE_FONT_ASSET_PATH, `${fileHash}${extMap[mime!] || ''}`)
+  fs.writeFileSync(finalFilePath, buffer)
+  return finalFilePath
+}
+
+export const loadFontAsset = (assetPath: string) => {
+  if (/^https?:\/\//i.test(assetPath)) return loadRemoteFontAsset(assetPath)
+  if (/^data:[^;]+;base64,/.test(assetPath)) return loadBase64Asset(assetPath)
+  return loadLocalFontAsset(assetPath)
 }
